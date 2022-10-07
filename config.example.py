b6db911a20f19c2664340159559658783f4b490d
tmonk <tmonk.hsieh@tmonk.tw>
2022-10-07 16:09:27 +0800

    add more config variable for podcast bot

--- config.example.py
+++ config.example.py
@@ -1,1 +1,10 @@
+# for Mastodon instance
+instance_post_url = ''
 access_token = ''
+
+# podcast channel main page.
+apple_podcast_page_url = ''
+google_podcasts_page_url = ''
+kkbox_page_url = ''
+soundcloud_page_url = ''
+spotify_page_url = ''
