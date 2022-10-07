a9609e10b34c273de4dd44986c8d71191fe31bab
tmonk <tmonk.hsieh@tmonk.tw>
2022-10-07 16:10:08 +0800

    initial crawlers.py file.

---
+++ crawlers.py
@@ -0,0 +1,14 @@
+def apple_podcast_crawler():
+    pass
+
+def google_podcasts_crawler():
+    pass
+
+def kkbox_crawler():
+    pass
+
+def soundcloud_crawler():
+    pass
+
+def spotify_crawler():
+    pass
